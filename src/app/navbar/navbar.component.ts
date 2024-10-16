1fddece65c11356a4250c493defbc140b6f785c8
nehru00123@gmail.com <nehru00123@gmail.com>
2024-10-16 08:49:49 +0530

    navbar added with component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { SidebarModule } from 'primeng/sidebar';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [ToolbarModule, ButtonModule, SplitButtonModule, InputTextModule, IconFieldModule, InputIconModule, SidebarModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  items: MenuItem[] | undefined;
+  sidebarVisible: boolean = false;
+  
+  ngOnInit() {
+      this.items = [
+          {
+              label: 'Update',
+              icon: 'pi pi-refresh'
+          },
+          {
+              label: 'Delete',
+              icon: 'pi pi-times'
+          }
+      ];
+  }
 
+  
 }
