42b96bbc644533bd19d9d129c3ff41e7f66e1217
nehru00123@gmail.com <nehru00123@gmail.com>
2024-10-16 09:37:54 +0530

    added styles

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { IconFieldModule } from 'primeng/iconfield';
@@ -7,13 +7,16 @@ import { InputTextModule } from 'primeng/inputtext';
 import { SidebarModule } from 'primeng/sidebar';
 import { SplitButtonModule } from 'primeng/splitbutton';
 import { ToolbarModule } from 'primeng/toolbar';
+import { EmulatedComponent } from './app.emulated';
+import { ShadowdomComponent } from './shadowdom.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ToolbarModule, ButtonModule, SplitButtonModule, InputTextModule, IconFieldModule, InputIconModule, SidebarModule],
+  imports: [ToolbarModule, ButtonModule, SplitButtonModule, InputTextModule, IconFieldModule, InputIconModule, SidebarModule, EmulatedComponent, ShadowdomComponent],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class NavbarComponent {
   items: MenuItem[] | undefined;
