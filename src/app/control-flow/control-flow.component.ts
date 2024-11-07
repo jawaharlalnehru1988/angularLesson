c2950da5eccff5403acce82c16064147898404a0
nehru00123@gmail.com <nehru00123@gmail.com>
2024-11-07 06:54:50 +0530

    control flow for loop

--- src/app/control-flow/control-flow.component.ts
+++ src/app/control-flow/control-flow.component.ts
@@ -1,9 +1,11 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-control-flow',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, NgFor],
   templateUrl: './control-flow.component.html',
   styleUrl: './control-flow.component.scss'
 })
@@ -22,6 +24,74 @@ dataObj = {
 age: number = 16;
 employee:string = "guesjdsflsdjf";
 
+fruitsDetails = [
+    {
+        id: 1,
+        name: 'apple',
+        price: 10
+    },
+    {
+        id: 2,
+        name: 'banana',
+        price: 20
+    },
+    {
+        id: 3,
+        name: 'orange',
+        price: 30
+    },
+    {
+        id: 4,
+        name: 'pear',
+        price: 40
+    },
+    {
+        id: 5,
+        name: 'strawberry',
+        price: 50
+    }
+];
+addFruit(){
+  this.fruitsDetails = [
+    {
+        id: 1,
+        name: 'apple',
+        price: 10
+    },
+    {
+        id: 2,
+        name: 'banana',
+        price: 20
+    },
+    {
+        id: 3,
+        name: 'orange',
+        price: 30
+    },
+    {
+        id: 4,
+        name: 'pear',
+        price: 40
+    },
+    {
+        id: 5,
+        name: 'strawberry',
+        price: 50
+    },
+    {
+        id: 6,
+        name: 'mango',
+        price: 60
+    }
+];
+}
 
+trackByFn(index: number, item:any) {
+  return item.id; 
+}
+
+emptyFruit(){
+  this.fruitsDetails = [];
+}
 
 }
