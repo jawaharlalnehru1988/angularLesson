8a4b214ad50d753cdf1b13d48f56992be33c8582
nehru00123@gmail.com <nehru00123@gmail.com>
2024-11-29 04:58:07 +0530

    validation added

--- src/app/formgroup/formgroup.component.ts
+++ src/app/formgroup/formgroup.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { CheckboxModule } from 'primeng/checkbox';
 import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-formgroup',
@@ -15,23 +16,50 @@ import { ButtonModule } from 'primeng/button';
   styleUrl: './formgroup.component.scss'
 })
 export class FormgroupComponent {
-  registerForm!:FormGroup;
+  registerForm!: FormGroup;
 
   constructor(private fb: FormBuilder){}
 
-  ngOnInit(){
+  ngOnInit() {
     this.initializeForm();
   }
-initializeForm(){
-  this.registerForm = this.fb.group({
-    username : [''],
-    comment : [''],
-    phone : [''],
-    terms : [false]
-  })
-}
-  onSubmit(){
-    console.log(this.registerForm.value);
+  initializeForm() {
+    this.registerForm = this.fb.group({
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20),
+          Validators.pattern('[a-zA-Z0-9 ]*'),
+        ],
+      ],
+      comment: ['', Validators.maxLength(100)],
+      phone: ['', [Validators.required,  Validators.pattern('^[6-9][0-9]{9}$')]],
+      terms: [false, Validators.requiredTrue],
+    });
+  }
+  onSubmit() {
     
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+      
+    }
+
+  }
+
+  getValidationError(userRegister:any){
+    if(userRegister?.hasError('required')){
+      return 'User name is required';
+    } else if(userRegister?.hasError('minlength')){
+      return 'User name must be at least 3 characters';
+    } else if(userRegister?.hasError('maxlength')){
+      return 'User name must be at most 20 characters';
+    } else if(userRegister?.hasError('pattern')){
+      return 'User name can only contain letters and numbers';
+    } else {
+      return '';
+    }
+
   }
 }
